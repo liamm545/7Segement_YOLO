2949eb17946368409e6f4e7b51441b27321f5888
liamm545 <100026444+liamm545@users.noreply.github.com>
2023-09-24 17:21:50 +0900

    Update functions.py
    1. conf가 가장 높은 bounding box의 넓이를 기준으로 넓이 필터링을 진행하며, 일정 비율 내외로만 허용

--- functions.py
+++ functions.py
@@ -1,30 +1,44 @@
 import cv2
 import numpy as np
 
-class YOLOProcessor:
+class SegmentDetector:
     def __init__(self):
         self.cls_to_str = ['-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    def enhance_dot_conf(self, items):
-        for i in range(1, len(items) - 1):
-            # Check if current item is a dot and if its neighbors are digits
-            if int(items[i][2]) == 1 and 2 <= int(items[i-1][2]) <= 11 and 2 <= int(items[i+1][2]) <= 11:
-                # Increase the confidence of the dot
-                items[i] = (1.0, items[i][1], items[i][2])
-        return items
+    @staticmethod
+    def calculate_areas(coor_list):
+        return [coor[2] * coor[3] for coor in coor_list]
 
-    def _get_detection_data(self, detect_result):
-        boxes = detect_result[0].boxes
-        return boxes.conf.cpu().numpy().tolist(), boxes.xywh.cpu().numpy().tolist(), boxes.cls.cpu().numpy().tolist()
+    def filter_boxes(self, conf_list, coor_list, cls_list, areas, threshold, area_ratio):
+        max_conf_index = conf_list.index(max(conf_list))
+        max_conf_area = areas[max_conf_index]
+        min_area = area_ratio[0] * max_conf_area
+        max_area = area_ratio[1] * max_conf_area
 
-    def process_detection(self, detect_result, threshold=0.65):
-        conf_list, coor_list, cls_list = self._get_detection_data(detect_result)
+        filtered_vals = []
+        for conf, coor, cls, area in zip(conf_list, coor_list, cls_list, areas):
+            if cls == 1.0:
+                if conf >= 0.3:
+                    filtered_vals.append((conf, coor, cls))
+                continue
+            if cls == 0.0:
+                filtered_vals.append((conf, coor, cls))
+                continue
+            if min_area <= area <= max_area and conf >= threshold:
+                filtered_vals.append((conf, coor, cls))
+        return filtered_vals
 
-        # Filter, sort, and map results
-        sorted_data = sorted(
-            filter(lambda item: item[0] >= threshold, zip(conf_list, coor_list, cls_list)),
-            key=lambda x: x[1][0]
-        )
-        result = ''.join([self.cls_to_str[int(item[2])] for item in sorted_data])
+    def process_detection(self, model_apply, threshold=0.6, area_ratio=(0.8, 1.2)):
+        detect_result = model_apply[0].boxes
+
+        conf_list = detect_result.conf.cpu().numpy().tolist()
+        coor_list = detect_result.xywh.cpu().numpy().tolist()
+        cls_list = detect_result.cls.cpu().numpy().tolist()
+
+        areas = self.calculate_areas(coor_list)
+        filtered_vals = self.filter_boxes(conf_list, coor_list, cls_list, areas, threshold, area_ratio)
+
+        sorted_vals = sorted(filtered_vals, key=lambda x: x[1][0])
+        result = ''.join([self.cls_to_str[int(item[2])] for item in sorted_vals])
 
         return result
